a56a27d246e3fc10eee8033e7fb323c08e60ec75
LuigiCasale95 <luigicasale95@gmail.com>
2025-08-29 16:26:57 +0200

    completamento esercizio

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -14,27 +14,33 @@ const fourthTeacher = teachers[3];
 console.log(fourthTeacher)
 
 // 2. Sostituisci il quinto insegnante nell'array teachers con 'Patrick'
-const profIndex = teachers.indexOf("Carlo")
-console.log(profIndex)
+const profIndex = teachers.indexOf("Carlo");
+console.log(profIndex);
 if (profIndex > -1) {
   teachers.splice(profIndex, 1, "Patrik")
 }
 console.log(teachers)
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-
+const lastTeacher = teachers.pop();
+console.log(lastTeacher);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = null;
+const firstTeacher = teachers.shift();
+console.log(firstTeacher)
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-
+teachers.push("Vanessa");
+console.log(teachers)
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-
+teachers.unshift("Sarah");
+console.log(teachers)
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis");
+console.log(lewisIndex)
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length === 0;
+console.log(isTeachersEmpty)
