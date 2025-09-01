8341549b17cc259a9e343ecafd21a04b91309619
LuigiCasale95 <luigicasale95@gmail.com>
2025-09-01 23:48:02 +0200

    prove del bonus

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -44,3 +44,63 @@ console.log(lewisIndex)
 // e salva il risultato nella variabile isTeachersEmpty
 const isTeachersEmpty = teachers.length === 0;
 console.log(isTeachersEmpty)
+
+
+
+
+
+/*********************************** ESERCITAZIONE IN CLASSE *********************************/
+// 1 L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore
+
+  let primoNumero = parseInt (prompt("Inserisci un numero per verificare il più grande"))
+  let secondoNumero = parseInt (prompt("Inserisci un numero per verificare il più grande")) 
+  console.log(primoNumero, secondoNumero)
+ 
+ if (primoNumero > secondoNumero) {
+  console.log("Il primo numero inserito è più grande ovvero " + primoNumero + " maggiore di " + secondoNumero)
+} else if (primoNumero < secondoNumero) {
+  console.log("Il secondo numero inserito è più grande ovvero " + secondoNumero + " maggiore di " + primoNumero)
+} else {
+  console.log("I numeri hanno la stessa grandezza")
+}
+
+// 2 L’utente inserisce due parole in successione, con due prompt.  Il software stampa prima la parola più corta, poi la parola più lunga.
+ let primaParola = prompt("Inserisci una parola")
+ let secondaParola = prompt("Inserisci una parola")
+
+if (primaParola.length > secondaParola.length) {
+  console.log(`La parola più corta è ***${secondaParola}*** quella più lunga è ***${primaParola}***`)
+} else if (primaParola.length < secondaParola.length) {
+    console.log(`La parola più corta è ***${primaParola}*** quella più lunga è ***${secondaParola}***`)
+} else {
+  console.log("Le parole hanno stessa la lunghezza")
+}
+
+/*3 Il software deve chiedere per 5 volte all’utente di inserire un numero.
+  Il programma stampa la somma di tutti i numeri inseriti. 
+  Esegui questo programma in due versioni, con il for e con il while (facoltativo)*/
+   let somma = 0
+for (let i = 0; i<=4; i++) {
+  let number = parseInt (prompt("Inserisce un numero per fare la somma"))
+    console.log("Numero inserito " + number)
+    somma = somma + number
+}
+console.log("il totale è " + somma)
+
+/*4 Crea un array vuoto. 
+ Chiedi per 6 volte all’utente di inserire un numero, 
+ se è dispari inseriscilo nell’array */
+
+ const contenitoreNumeriDispari = []
+
+ for (let i = 0; i<=5; i++) {
+  let number = parseInt (prompt("Inserisce un numero per verificare se dispari"))
+  console.log("numeri inseriti " + number)
+
+  
+  if (number % 2 === 1) {
+    contenitoreNumeriDispari.push(number)
+
+  }
+ }
+     console.log(`I numeri dispari sono ${contenitoreNumeriDispari}`)
--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -10,17 +10,42 @@ const teachers = [
 
 // 1. Inverti l'ordine degli insegnanti nell'array teachers
 // e salva il risultato nella variabile reversedTeachers
-const reversedTeachers = null;
+const reversedTeachers = [];
+console.log(teachers.length)
+for (i = teachers.length -1; i >= 0 ; i--){
+  reversedTeachers.push(teachers[i])
+} 
+console.log(reversedTeachers)
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+
+/* const longNames = [];
+for (i = 0; i <= teachers.length; i++) {
+  const nome = teachers[i]
+  if (nome.length > 5) {
+    longNames.push(nome)
+  }
+}
+console.log(longNames) */
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+/* const edIndex = teachers.indexOf("Ed")
+console.log(edIndex)
+
+teachers.splice(edIndex, 1)
+console.log(teachers)
+ */
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+const isFabioPresent = fabio;
+if (teachers.indexOf(fabio) !== -1) {
+  console.log("Il nume è presente") 
+}
+  else {
+    console.log("Il nome non è presente")
+  }
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
 const teachersString = null;
